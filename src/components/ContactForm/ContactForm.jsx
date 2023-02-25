d1f17d3c8382b2b95d835a2cd9e800b581878df4
DarynaF <karlu4ka333@gmail.com>
2023-02-25 16:57:16 +0300

    to checkout

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,77 +1,49 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { addContact } from 'redux/slice';
+import { useDispatch } from 'react-redux/es/exports';
 import { FcTwoSmartphones } from 'react-icons/fc';
-import PropTypes from 'prop-types';
+import { Formik, Form, Field } from 'formik';
 import s from './contactForm.module.css';
 
-const ContactForm = ({ onSubmit }) => {
-  const [name, setName] = useState('');
-  const [number, setNumber] = useState('');
+const ContactForm = () => {
+  const dispatch = useDispatch();
 
-  const handleChange = event => {
-    const { name, value } = event.target;
-
-    switch (name) {
-      case 'name':
-        setName(value);
-        break;
-
-      case 'number':
-        setNumber(value);
-        break;
-
-      default:
-        return;
-    }
-  };
-
-  const handleSubmit = event => {
-    event.preventDefault();
-    onSubmit({ name, number });
-    reset();
-  };
-
-  const reset = () => {
-    setName('');
-    setNumber('');
+  const handleSubmit = ( {name, number}, { resetForm }) => {
+    dispatch(addContact({ name, number }));
+    resetForm();
   };
 
   return (
-    <form className={s.submit} onSubmit={handleSubmit}>
-      <label className={s.label}>
-        Name
-        <input
-          className={s.input}
-          type="text"
-          name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          value={name}
-          onChange={handleChange}
-          required
-        />
-      </label>
-      <label className={s.label}>
-        Number
-        <input
-          className={s.input}
-          type="tel"
-          name="number"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          value={number}
-          onChange={handleChange}
-          required
-        />
-      </label>
-      <button className={s.btn} type="submit" disabled={!name && !number}>
-        <span>Add contact</span> <FcTwoSmartphones />
-      </button>
-    </form>
+    <Formik initialValues={{ name: '', number: '' }} onSubmit={handleSubmit}>
+      <Form className={s.submit}>
+        <label className={s.label}>
+          Name
+          <Field
+            className={s.input}
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </label>
+        <label className={s.label}>
+          Number
+          <Field
+            className={s.input}
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+        </label>
+        <button className={s.btn} type="submit">
+          <span>Add contact</span> <FcTwoSmartphones />
+        </button>
+      </Form>
+    </Formik>
   );
-};
-
-ContactForm.propTypes = {
-  onSubmit: PropTypes.func.isRequired,
 };
 
 export default ContactForm;
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,34 +1,35 @@
 import React from 'react';
-import { FcFullTrash } from "react-icons/fc";
-import PropTypes from 'prop-types';
+import { useSelector, useDispatch } from 'react-redux/es/exports';
+import { getVisibleContacts } from 'redux/selectors';
+import { deleteContact } from 'redux/slice';
+import { FcFullTrash, FcBusinessman } from 'react-icons/fc';
 import s from './contactList.module.css';
 
-const ContactList = ({ contacts, onDeleteContact }) => (
-  <ul className={s.list}>
-    {contacts.map(({ id, name, number }) => (
-      <li className={s.contact} key={id}>
-        <p>{name}:</p>
-        <p>{number}</p>
-        <button
-          className={s.btn}
-          type="button"
-          onClick={() => onDeleteContact(id)}
-        >
-          <span>Delete</span> <FcFullTrash/>
-        </button>
-      </li>
-    ))}
-  </ul>
-);
+const ContactList = () => {
+  const dispatch = useDispatch();
 
-ContactList.propTypes = {
-  contacts: PropTypes.arrayOf
-    (PropTypes.exact({
-    name: PropTypes.string.isRequired,
-    number: PropTypes.string.isRequired,
-    id: PropTypes.string.isRequired,
-    })),
-  onDeleteContact: PropTypes.func.isRequired,
+  const contacts = useSelector(getVisibleContacts);
+
+  return (
+    <ul className={s.list}>
+      {contacts.map(({ id, name, number }) => (
+        <li className={s.contact} key={id}>
+          <span>
+            <FcBusinessman />{' '}
+          </span>
+          <p>{name}:</p>
+          <p>{number}</p>
+          <button
+            className={s.btn}
+            type="button"
+            onClick={() => dispatch(deleteContact({ id }))}
+          >
+            <span>Delete</span> <FcFullTrash />
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default ContactList;
